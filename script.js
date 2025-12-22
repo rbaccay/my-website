16e615c84ee57348fade4d6896f5a81b7cd6451e
rbaccay <rizabaccay16@gmail.com>
2025-12-22 11:05:24 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 return;
             }
             localStorage.setItem("studentName", nameInput.value.trim());
-            window.location.href = "index.html";
+            window.location.href = "SecondPage.html";
         };
     }
 
